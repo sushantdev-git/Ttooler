f4607eda1c64739fbcede4659abe9bc2f8f9bf1f
Sushant <sushantkumar6700@gmail.com>
2021-07-28 20:07:16 +0530

    added home screen greeting widget

---
+++ lib/widgets/home/info_greeting.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InfoGreeting extends StatelessWidget {
+  const InfoGreeting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Random", style: TextStyle(
+                fontSize: 25,
+              ),),
+              Text("Good Morning", style: TextStyle(
+                fontWeight: FontWeight.w100,
+                fontSize: 30,
+              ),),
+            ],
+          ),
+          CircleAvatar(
+            radius: 35,
+            backgroundColor: Theme.of(context).backgroundColor,
+          )
+        ],
+      ),
+    );
+  }
+}
