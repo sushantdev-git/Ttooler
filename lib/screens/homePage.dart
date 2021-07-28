3b73e8b6ed6fa068f541aa92c72e8b09bfefd7f8
Sushant <sushantkumar6700@gmail.com>
2021-07-28 21:24:05 +0530

    added global key to open the drawer without appbar

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ttooler/widgets/drawer.dart';
 import 'package:ttooler/widgets/hamburgerButton.dart';
 import 'package:ttooler/widgets/home/info_greeting.dart';
 
@@ -11,29 +12,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  //we can use this key to trigger the drawer without using appBar
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
+        key: _scaffoldKey,
+          drawer: DrawerWidget(),
           body: Container(
-        height: mediaQuery.height,
-        width: mediaQuery.width,
-        padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(child: HamburgerButton(), onTap: (){
-                Navigator.of(context).pop();
-              },
-              borderRadius: BorderRadius.circular(5),
+            height: mediaQuery.height,
+            width: mediaQuery.width,
+            padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    child: HamburgerButton(),
+                    onTap: () {
+                      _scaffoldKey.currentState!.openDrawer();
+                    },
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  InfoGreeting()
+                ],
               ),
-              InfoGreeting()
-            ],
-          ),
-        ),
-      )),
+            ),
+          )),
     );
   }
 }
