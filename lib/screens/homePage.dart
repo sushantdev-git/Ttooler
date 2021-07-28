cc56396916d958ecd25e786975ae6374aefbef3c
Sushant <sushantkumar6700@gmail.com>
2021-07-28 20:08:17 +0530

    added hambuger button to home page

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ttooler/widgets/hamburgerButton.dart';
+import 'package:ttooler/widgets/home/info_greeting.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,11 +13,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("how are you"),
-      ),
+    final mediaQuery = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+          body: Container(
+        height: mediaQuery.height,
+        width: mediaQuery.width,
+        padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(child: HamburgerButton(), onTap: (){
+                Navigator.of(context).pop();
+              },
+              borderRadius: BorderRadius.circular(5),
+              ),
+              InfoGreeting()
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
