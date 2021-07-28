1a47be70d188a0d1ca77ea1bbb0d4d5106333223
Sushant <sushantkumar6700@gmail.com>
2021-07-28 15:06:15 +0530

    added gitignore file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ttooler/screens/landingPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Text("hello how are you"),
 
-          ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: LandingPage(),
+      theme: ThemeData(
+        primaryColor:Colors.white,
+        canvasColor: Color(0xff181920),
+        fontFamily: "OpenSans",
+        textTheme: TextTheme(
+          bodyText1: TextStyle(),
+          bodyText2: TextStyle(),
+        ).apply(
+          bodyColor: Colors.white,
         ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            primary: Colors.white,
+            side: BorderSide(width: 1, color: Colors.white),
+          )
+        )
+      ),
       routes: {
 
       },
--- lib/screens/landingPage.dart
+++ lib/screens/landingPage.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class StartPage extends StatelessWidget {
-  const StartPage({Key? key}) : super(key: key);
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +12,53 @@ class StartPage extends StatelessWidget {
         margin: EdgeInsets.only(top:mediaQeury.size.height/10, left: mediaQeury.size.width/10, right:mediaQeury.size.width/10 ),
         height: mediaQeury.size.height,
         width: mediaQeury.size.width,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: mediaQeury.size.height/3,
-              width: mediaQeury.size.height/3,
-              child: Center(child: Image.asset("assets/images/tree.png", fit: BoxFit.cover,)),
-            ),
-
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: mediaQeury.size.height/3,
+                width: mediaQeury.size.height/3,
+                child: Center(child: Image.asset("assets/images/tree.png", fit: BoxFit.cover,)),
+              ),
+              SizedBox(height: 20,),
+              Text("Ttooler", style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.w700,
+                color: Colors.white
+              ),),
+              SizedBox(
+                height: 40,
+              ),
+              Text("Welcome", style: TextStyle(
+                fontWeight: FontWeight.w100,
+                fontSize: 35,
+              ),),
+              SizedBox(
+                height: 10,
+              ),
+              Text("Ttooler is powerful management app for students.", softWrap: true, style: TextStyle(
+                color: Colors.white54,
+                fontSize: 21,
+              ), textAlign: TextAlign.center,),
+              SizedBox(
+                height: 40,
+              ),
+              OutlinedButton(onPressed: (){}, child: Container(
+                width: mediaQeury.size.width/3,
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Get started",),
+                    Icon(Icons.arrow_forward),
+                  ],
+                ),
+              ),
+              )
+            ],
+          ),
         ),
       ),
     );
