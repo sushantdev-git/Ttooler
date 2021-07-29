d9d3eda3fe6b2a014a64a851507f168c77e1ff09
Sushant <sushantkumar6700@gmail.com>
2021-07-29 16:48:22 +0530

    refactored widget so to set different glance

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ttooler/widgets/buttons/iconButton.dart';
+import 'package:ttooler/konstant/konstant.dart';
 import 'package:ttooler/widgets/drawer.dart';
 import 'package:ttooler/widgets/buttons/hamburgerButton.dart';
+import 'package:ttooler/widgets/home/glanceHeader.dart';
 import 'package:ttooler/widgets/home/info_greeting.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,78 +17,79 @@ class _HomePageState extends State<HomePage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   //we can use this key to trigger the drawer without using appBar
 
+  GlanceType _glanceType = GlanceType.Todo;
+
+  void setGlanceType(GlanceType type){
+    setState(() {
+      _glanceType = type;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-          key: _scaffoldKey,
-          drawer: DrawerWidget(),
-          body: Container(
-            height: mediaQuery.height,
-            width: mediaQuery.width,
-            padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  InkWell(
-                    child: HamburgerButton(),
-                    onTap: () {
-                      _scaffoldKey.currentState!.openDrawer();
-                    },
-                    borderRadius: BorderRadius.circular(10),
-                    highlightColor: Colors.transparent, //this line will remove the highlight color of inkwell
+        key: _scaffoldKey,
+        drawer: DrawerWidget(),
+        body: Container(
+          height: mediaQuery.height,
+          width: mediaQuery.width,
+          padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: HamburgerButton(),
+                  onTap: () {
+                    _scaffoldKey.currentState!.openDrawer();
+                  },
+                  borderRadius: BorderRadius.circular(10),
+                  highlightColor: Colors
+                      .transparent, //this line will remove the highlight color of inkwell
+                ),
+                InfoGreeting(),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("At Glance,"),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: Stack(
+                    children: [
+                      GlanceHeader(glanceType: _glanceType, changeGlance: setGlanceType,),
+                      TodoGlance(),
+                    ],
                   ),
-                  InfoGreeting(),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("At Glance,"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 5),
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Container(
-                                        height: 50,
-                                        child: Image.asset(
-                                            "assets/images/icon_images/todo_icon.png"),
-                                      ),
-                                      SizedBox(width: 10,),
-                                      Text(
-                                        "Todo",
-                                        style: TextStyle(
-                                          fontSize: 30,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  BorderIconButton(icon: Icons.east, onPress: (){})
-                                ],
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
+
+class TodoGlance extends StatelessWidget {
+  const TodoGlance({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
+
