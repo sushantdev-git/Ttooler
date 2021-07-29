1f4095557c5d74a5766c59e0647cdc66f7e8b32a
Sushant <sushantkumar6700@gmail.com>
2021-07-29 16:46:54 +0530

    added a constant file so we can use some value directly from there

---
+++ lib/konstant/konstant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+enum GlanceType{
+  Todo,
+  Reminder,
+  TimeTable
+}
+
+final glanceTypeHeadingStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+
