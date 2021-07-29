fc1026d7a39684c8eb080b91739c8a6a68425459
Sushant <sushantkumar6700@gmail.com>
2021-07-29 11:53:38 +0530

    added a custom border icon button

---
+++ lib/widgets/buttons/iconButton.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class BorderIconButton extends StatelessWidget {
+  
+  final Function onPress;
+  final IconData icon;
+  const BorderIconButton({required this.icon, required this.onPress});
+
+  @override 
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(100),
+      onTap: () => onPress,
+      child: Container(
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.white54),
+            borderRadius: BorderRadius.circular(100)
+        ),
+          child: Icon(icon, color: Colors.white,),
+      ),
+    ) ;
+  }
+}
