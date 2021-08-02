f12dd64aba61f3ce12c8cd614eb6a23d5b326f88
Sushant <sushantkumar6700@gmail.com>
2021-08-02 18:46:45 +0530

    added change notifier provider and some changes in theme:

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:ttooler/modals/todoProvider.dart';
 import 'package:ttooler/pages/landingPage.dart';
 
 void main() {
@@ -19,41 +21,56 @@ class MyApp extends StatelessWidget {
         statusBarColor: Color(0xff181920)
     )); //this will set color of status bar
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home: LandingPage(),
-      theme: ThemeData(
-        primaryColor:Color(0xff181920),
-        accentColor: Colors.white,
-        backgroundColor: Color(0xff262A3D),
-        canvasColor: Color(0xff181920),
-        fontFamily: "OpenSans",
-        textTheme: TextTheme(
-          bodyText1: TextStyle(),
-          bodyText2: TextStyle(),
-        ).apply(
-          bodyColor: Colors.white,
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            primary: Colors.white,
-            side: BorderSide(width: 1, color: Colors.white),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15)
-            ),
-            tapTargetSize: MaterialTapTargetSize.padded,
-          )
-        ),
-        textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
+    return ChangeNotifierProvider(
+      create: (context) => TodoProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+          home: LandingPage(),
+        theme: ThemeData(
+          primaryColor:Color(0xff181920),
+          accentColor: Color(0xffb1acfa),
+          backgroundColor: Color(0xff262A3D),
+          canvasColor: Color(0xff181920),
+          fontFamily: "OpenSans",
+          textTheme: TextTheme(
+            bodyText1: TextStyle(),
+            bodyText2: TextStyle(),
+          ).apply(
+            bodyColor: Colors.white,
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
               primary: Colors.white,
-              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              side: BorderSide(width: 1, color: Colors.white),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)
+              ),
+              tapTargetSize: MaterialTapTargetSize.padded,
             )
-        )
-      ),
-      routes: {
+          ),
+          textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              )
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            contentPadding: EdgeInsets.all(15),
+              labelStyle: TextStyle(
+                  color: Colors.white
+              ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xff363a55),
+              padding: EdgeInsets.all(15),
+            )
+          )
 
-      },
+        ),
+
+
+      ),
     );
   }
 }
