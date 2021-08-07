e1762076ce24893b6500c55fe4dc7e63ac54b432
Sushant <sushantkumar6700@gmail.com>
2021-08-07 20:59:03 +0530

    added some extra paremater in iconbutton to create unqiue hero tag

--- lib/widgets/buttons/iconButton.dart
+++ lib/widgets/buttons/iconButton.dart
@@ -5,22 +5,40 @@ class BorderIconButton extends StatelessWidget {
   
   final Function onPress;
   final IconData icon;
-  const BorderIconButton({required this.icon, required this.onPress});
+  final String belongTo;
+  final String type;
+  final int index;
+  const BorderIconButton({required this.icon, required this.onPress, required this.belongTo, required this.type, required this.index});
+
 
   @override 
   Widget build(BuildContext context) {
+    final String heroTag = belongTo+type;
+    print(heroTag);
     return InkWell(
       borderRadius: BorderRadius.circular(100),
       onTap: (){
+        if(icon == Icons.edit) {
+          onPress(heroTag);
+          return;
+        }
         onPress();
       },
-      child: Container(
-        padding: EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          border: Border.all(width: 2, color: Colors.white54),
-            borderRadius: BorderRadius.circular(100)
+      child: Hero(
+        tag: heroTag,
+        child: Material(
+          elevation: 5,
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              // border: Border.all(width: 2, color: Colors.white54),
+                color: Color(0xffb1acfa),
+                borderRadius: BorderRadius.circular(10)
+            ),
+              child: Icon(icon, color: Color(0xff262A3D), size: 20,),
+          ),
         ),
-          child: Icon(icon, color: Colors.white,),
       ),
     ) ;
   }
