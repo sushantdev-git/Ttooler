4f3cf9cb2b74c84da1d9eb54e9ca46a5279c3b3d
Sushant <sushantkumar6700@gmail.com>
2021-08-07 21:11:32 +0530

    added reminder fab which is same as todo fab

---
+++ lib/widgets/reminder/reminderFAB.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:ttooler/pageRoutebuilder/heroPageRouteBuilder.dart';
+import 'package:ttooler/pages/todoPage.dart';
+import 'package:ttooler/widgets/reminder/reminderInputCard.dart';
+
+class ReminderFloatingAB extends StatelessWidget {
+  const ReminderFloatingAB({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.all(10), child: GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(HeroDialogRoute(builder: (context) {
+          return AddReminderPopupCard(heroTag: "ReminderButton", title: "", subtitle: "", description: "", datetime: null, cardTitle: "Add",);
+        },));
+      },
+      child: Hero(
+        tag: "ReminderButton",
+        createRectTween: (begin, end) {
+          return CustomRectTween(begin: begin as Rect, end: end as Rect);
+        },
+        child: Material(
+          elevation: 2,
+          color: Theme.of(context).accentColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+          child: const Icon(
+            Icons.add,
+            size: 56,
+            color: Color(0xff262A3D),
+          ),
+        ),
+      ),
+    ),);
+  }
+}
